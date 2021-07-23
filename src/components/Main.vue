fa89e405ee07d7485c65d2a953cad5aed91e2331
Hayoung <gkduddy0814@gmail.com>
2021-07-24 00:07:08 +0900

    feat : test vue

---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>msg : {{ msg }}</h2>
+  <div>computed : {{ shortMessage }}</div>
+  <div>{{ now }}</div>
+  <div id="example-1" v-if="awesome">Vue is awesome!</div>
+  <div v-else>no, bbbb</div>
+</template>
+
+<script>
+export default {
+  // vue 파일의 컴포넌트화는 이런식으로 하는듯?!!?!?!
+  name: 'Main',
+  props: {
+    msg: String,
+  },
+
+  // computed 를 통해 복잡한 로직을 함수로 리턴해주기!
+  computed: {
+    shortMessage: function () {
+      return this.msg.substring(10);
+    },
+    now: function () {
+      return new Date().toString().substring(0, 15);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/components/ReportList.vue
@@ -0,0 +1,12 @@
+<template>
+  <li>
+    {{ stage }}, {{ result }} <button @click="$emit('remove')">remove</button>
+    <!--  emit함수를 통해서 자식이 부모에게 변화시키는 데이터 보내는 느낌 -->
+  </li>
+</template>
+
+<script>
+export default {
+  props: { stage: String, result: String },
+};
+</script>
